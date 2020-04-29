7bb3d26ed96947a6800d46df32cb0f1788005b06
Daniel Nikolov <dnikolov@gmail.com>
2020-04-29 11:06:37 +0300

    Initial project skeleton

---
+++ src/main/java/org/wepower/pricecalculator/PricecalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.wepower.pricecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricecalculatorApplication.class, args);
+	}
+
+}
